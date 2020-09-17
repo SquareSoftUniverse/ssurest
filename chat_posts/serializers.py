f9f7c1df1a2988ababc1f0cf665b4b61b48d4a6d
Aaron W <aawiegel@gmail.com>
2020-09-17 09:09:52 -0700

    exclude password

--- chat_posts/serializers.py
+++ chat_posts/serializers.py
@@ -6,7 +6,7 @@ class ChatUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ChatUsers
-        fields = '__all__'
+        exclude = ('password', )
 
 
 class HandleSerializer(serializers.ModelSerializer):
